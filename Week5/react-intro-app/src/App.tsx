49938d7e182b58e05eed27d278ae107c76197a3e
jdelacruz96 <jonathan.delacruz@revature.com>
2024-08-08 12:44:01 -0400

    finished context stuff for now

--- Week5/react-intro-app/src/App.tsx
+++ Week5/react-intro-app/src/App.tsx
@@ -1,19 +1,23 @@
 import Counter from "./components/Counter/Counter";
+import UserStatus from "./components/UserStatus/UserStatus";
+import { UserProvider } from "./UserContext";
 
 function App() {
 
   return (
+
     
-   <div>
-      {/*Displaying my *custom* Counter component! Notice that unlike the HTML div element tag, I use a 
+      <div>
+        {/*Displaying my UserStatus component.*/}
+        <UserProvider>
+          <UserStatus />
+        </UserProvider>
+        {/*Displaying my *custom* Counter component! Notice that unlike the HTML div element tag, I use a 
         capital letter to start off my react component name!*/}
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      <Counter />
-      
-   </div>
+        <Counter />
+      </div>
+  
+   
   )
 }
 
--- Week5/react-intro-app/src/components/UserStatus/UserStatus.tsx
+++ Week5/react-intro-app/src/components/UserStatus/UserStatus.tsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React, {useState} from 'react'
+
+import { useUser } from '../../UserContext'
+
 
 function UserStatus() {
+    //Accesing the context, via our useUser hook function that we created in UserContext.tsx 
+    const { user, login, logout } = useUser();
+
+    //Storing the user inputted username (from our text input element below) in this component's state.
+    //Components can leverage both an individual state AND a context. Store information wherever it makes the most
+    //sense. Not everything needs to (or should be) stored in a context. 
+    const [username, setUsername] = useState<string>('');
+
   return (
-    <div>UserStatus</div>
+    <div>
+        {/*We will show different page content based on whether or not a user is logged in*/}
+        {user ? (
+            <div>
+                <p>Welcome, {user}!</p>
+                {/*If the user is currently logged in, we will display a logout button for them*/}
+                <button onClick={logout}>Logout</button>
+            </div>
+        ) : (
+            <div>
+                <input 
+                    type="text" 
+                    placeholder="Enter Username"
+                    value={username}
+                    onChange={(e) => setUsername(e.target.value)}
+                />
+                {/*Login button that calls login function from UserContext*/}
+                <button onClick={() => login(username)}>Login</button>
+            </div>
+        )}
+    </div>
   )
 }
 
-export default UserStatus
+export default UserStatus;

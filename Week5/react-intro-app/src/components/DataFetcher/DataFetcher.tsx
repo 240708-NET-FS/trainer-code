b9980039a6b5475f42d6d335c1be64879fe8a88b
jdelacruz96 <jonathan.delacruz@revature.com>
2024-08-09 10:10:19 -0400

    adding my datafetching and post render components

---
+++ Week5/react-intro-app/src/components/DataFetcher/DataFetcher.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function DataFetcher() {
+
+
+
+    
+  return (
+    <div>DataFetcher</div>
+  )
+}
+
+export default DataFetcher
---
+++ Week5/react-intro-app/src/components/PostList/PostList.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function PostList() {
+
+    
+  return (
+    <div>PostList</div>
+  )
+}
+
+export default PostList

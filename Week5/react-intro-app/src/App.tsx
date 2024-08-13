78f03689b2f211c2350ae8b2975ce401b112a671
jdelacruz96 <jonathan.delacruz@revature.com>
2024-08-13 10:56:24 -0400

    finished router

--- Week5/react-intro-app/src/App.tsx
+++ Week5/react-intro-app/src/App.tsx
@@ -3,26 +3,54 @@ import UserStatus from "./components/UserStatus/UserStatus";
 import DataFetcher from "./components/DataFetcher/DataFetcher";
 import FormWithReducer from "./components/FormWithReducer/FormWithReducer";
 import { UserProvider } from "./UserContext";
+import { BrowserRouter, Route, Routes, Link } from "react-router-dom";
 
 function App() {
 
   return (
 
-    
+    <BrowserRouter>
       <div>
-        {/*Displaying my UserStatus component.*/}
-        <UserProvider>
-          <UserStatus />
-        </UserProvider>
-        {/*Displaying my *custom* Counter component! Notice that unlike the HTML div element tag, I use a 
-        capital letter to start off my react component name!*/}
-        <Counter />
-        {/*Displaying my DataFetcher and it's child PostList that is rendered inside of it */}
-        <DataFetcher />
-        {/*Displaying my form that leverages a reducer*/}
-        <FormWithReducer />
+
+          {/*Using a simple nav menu to better illustrate BrowserRouter functionality */}
+          <nav>
+            <ul>
+              <li>
+                  <Link to='/'>Home</Link>
+              </li>
+              <li>
+                  <Link to='/counter'>Counter</Link>
+              </li>
+              <li>
+                  <Link to='/data-fetcher'>Forum Posts</Link>  
+              </li>
+              <li>
+                  <Link to='/form'>Form with Reducer</Link>
+              </li>
+            </ul>
+          </nav>
+
+          {/*Defining Routes for my components, as well as a default/root route */}
+      
+            <Routes>
+              <Route path="/" 
+              element={<UserProvider>
+                          <UserStatus/>
+                      </UserProvider>}/>
+              
+              
+              <Route path="/counter" element={<Counter/>}/>
+              <Route path="/data-fetcher" element={<DataFetcher/>}/>
+              <Route path="/form" element={<FormWithReducer/>}/>
+            </Routes>
       </div>
-  
+
+
+
+
+
+
+    </BrowserRouter>
    
   )
 }
